accb2a080a391cafc188db272509fb607267f750
Adroaldo <hygino@msn.com>
2022-03-16 20:43:39 -0300

    UseRef

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,6 +1,8 @@
-import { useCallback, useMemo, useState } from 'react';
+import { useCallback, useMemo, useRef, useState } from 'react';
 
 export const Login = () => {
+
+    const inputPasswordRef = useRef<HTMLInputElement>(null);
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -14,6 +16,9 @@ export const Login = () => {
     const handleEntrar = useCallback(() => {
         console.log(email);
         console.log(password);
+        if (inputPasswordRef.current !== null) {
+            inputPasswordRef.current.focus();
+        }
     }, [email, password]);
 
 
@@ -24,12 +29,23 @@ export const Login = () => {
                 <p>Quantidade de caracteres no Email: {emailLenght}</p>
                 <label>
                     <span>Email</span>
-                    <input type='email' value={email} onChange={e => setEmail(e.target.value)} />
+                    <input 
+                        type='email' 
+                        value={email} 
+                        onChange={e => setEmail(e.target.value)}
+                        onKeyDown= {e => e.key==='Enter' ? inputPasswordRef.current?.focus() : undefined}
+                    />
                 </label>
                 <label>
                     <span>Senha</span>
-                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
+                    <input
+                        type='password'
+                        value={password}
+                        ref={inputPasswordRef}
+                        onChange={e => setPassword(e.target.value)}
+                    />
                 </label>
+
                 <button type='button' onClick={handleEntrar}>
                     Entrar
                 </button>
