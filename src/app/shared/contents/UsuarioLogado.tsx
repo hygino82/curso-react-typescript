e55165c2edb78b5ddb9e6bfc46ac63378bd821b3
Adroaldo <hygino@msn.com>
2022-03-17 10:34:31 -0300

    Passando funções no contexto

--- src/app/shared/contents/UsuarioLogado.tsx
+++ src/app/shared/contents/UsuarioLogado.tsx
@@ -1,14 +1,20 @@
-import { createContext } from "react";
+import { createContext, useCallback } from "react";
 
 interface IUsuarioLogadoContextData {
     nomeDoUsuario: string;
+    logout: () => void;
 }
 
 export const UsuarioLogadoContext = createContext<IUsuarioLogadoContextData>({} as IUsuarioLogadoContextData);
 
 export const UsuarioLogadoProvider: React.FC = ({ children }) => {
+
+    const handleLogOut = useCallback(() => {
+        console.log('Logout executou');
+    }, []);
+
     return (
-        <UsuarioLogadoContext.Provider value={{ nomeDoUsuario: 'Dilma' }}>
+        <UsuarioLogadoContext.Provider value={{ nomeDoUsuario: 'Dilma', logout: handleLogOut }}>
             {children}
         </UsuarioLogadoContext.Provider>
     );
